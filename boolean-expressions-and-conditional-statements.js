582062e8e4c0edb025eb644eafc4d24e0334eca9
pujajathar <pujajathar91@.com>
2026-04-22 15:31:36 -0500

    my game

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -28,19 +28,45 @@ const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+const hasSword = false;
+const hasCompass = true;
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
 const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+//let choice1 = readline.question("Do you have a 'sword'? Please type your answer in 'Yes' or 'No' ");
 
-if (choice === "mountains" && hasTorch) {
-  console.log("You safely navigate through the dark mountains.");
-} else if (choice === "mountains" && !hasTorch) {
-  console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
-  console.log("You find your way to the village.");
-} else {
-  console.log("You get lost and wander aimlessly.");
-}
+if (choice === "mountains")
+        {
+
+        let choice1 = readline.question("Do you have a 'sword'? Please type your answer in 'Yes' or 'No' ");
+    if (choice1 === "No") 
+         {
+
+         console.log("You can safely navigate through the dark mountains but if you see a danger, then run!");
+         }
+         else if (choice === "mountains" && hasTorch && choice1 === "Yes")
+        {
+         console.log("You safely navigate through the dark mountains and ready to defend when sense a danger.");
+        }}
+    else if (choice === "mountains" && !hasTorch) 
+        {
+         console.log("It's too dark to proceed. You decide to turn back.");
+        } 
+    else if (choice === "village")
+        {
+            if(!hasCompass || hasMap)
+            {
+             console.log("You find your way to the village.");
+            } 
+            else 
+            {
+                console.log("Finding a way to village is difficult and you need to work harder.");
+            }
+        }   
+    else 
+            {
+         console.log("You get lost and wander aimlessly.");
+            }
 
 /* 
 
@@ -49,4 +75,4 @@ Add Customization and expand the game:
   - Include additional items (e.g., a sword, a compass).
   - Use nested conditionals and logical operators to create complex outcomes.
 
-*/
+*/ 
